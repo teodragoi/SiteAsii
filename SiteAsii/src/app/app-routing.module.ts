aed570b909a1af750321445ca56b05b00096a637
Eddy <irimia.eduard@yahoo.com>
2020-04-13 11:32:35 +0300

    home page

--- SiteAsii/src/app/app-routing.module.ts
+++ SiteAsii/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/home/home.component';
+const routes: Routes = [
+{
+  path: '',
+  component: HomeComponent
+},
 
-
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
