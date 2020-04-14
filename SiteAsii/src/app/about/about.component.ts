3f26f042babd6daa17be7dc77e7f55294797fd4e
IoanaMP <55753020+IoanaMP@users.noreply.github.com>
2020-04-14 13:39:04 +0300

    asta era?

--- SiteAsii/src/app/about/about.component.ts
+++ SiteAsii/src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-asii-about',
+  selector: 'asii-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
