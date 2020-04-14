7d577c5814104e76a5b740a2667d5e78a1ca73dd
Ioana Pînzariu <55753020+IoanaMP@users.noreply.github.com>
2020-04-14 15:07:07 +0300

    sper ca e ok... (#2)
    * sper ca e ok...incep sa citesc tot mai mult despre centrat chestii :)
    
    * asta era?
    
    * ok so fara material in modul separat..

--- SiteAsii/src/app/app.module.ts
+++ SiteAsii/src/app/app.module.ts
@@ -1,12 +1,13 @@
+import { AboutComponent } from './about/about.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
+import { MatCardModule } from '@angular/material/card';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,8 @@ import { HeaderComponent } from './header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
     MatButtonModule
   ],
   providers: [],
