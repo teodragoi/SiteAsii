a91e01f10872e147b83f4b0d56971c8c3689f53f
Eddy <irimia.eduard@yahoo.com>
2020-04-28 12:18:07 +0300

    home page after comments

--- SiteAsii/src/app/app-routing.module.ts
+++ SiteAsii/src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MembriiComponent } from 'src/app/membrii/membrii.component';
 const routes: Routes = [
-  {
-    path: 'membrii', component: MembriiComponent,
-  },
 ];
 
 @NgModule({
