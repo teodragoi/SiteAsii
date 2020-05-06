85ed9ded9435cadd53f056e99a8087a4bf8c8cee
LaurStef <56784662+LaurStef@users.noreply.github.com>
2020-05-06 04:24:52 +0300

    added dropdown with a variable that save the selected option

--- SiteAsii/src/app/app.module.ts
+++ SiteAsii/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { DepartmentsComponent } from './departments/departments.component';
 import { MyPopupComponent } from './my-popup/my-popup.component';
 import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
     MembriiComponent,
     MemberTileComponent,
     DepartmentsComponent,
-    MyPopupComponent
+    MyPopupComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
