abb692fd0e02797afe6b108287626ffd2144c049
Teo Dragoi <44653077+teodragoi@users.noreply.github.com>
2020-10-22 21:54:16 +0300

    Animations (#16)
    * Added popup component and transmited department to it
    
    * Added department popup, members dropdown and members section
    
    * Moved member tile in memebrs component and renamed Dropdown to DepartmentDropdown
    
    * Added projects carousel and links in headers
    
    * Added member model and changed member tile to get input
    
    * Added smooth scroll and animation on header

--- SiteAsii/src/app/header/header.component.ts
+++ SiteAsii/src/app/header/header.component.ts
@@ -1,15 +1,36 @@
-import { ViewportScroller } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'asii-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private viewportScroller: ViewportScroller) {}
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    document.querySelector('.header-bar').classList.remove('scrolled');
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollOffset =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop ||
+      0;
+
+    if (scrollOffset <= 500) {
+      document.querySelector('.header-bar').classList.remove('scrolled');
+    } else {
+      document.querySelector('.header-bar').classList.add('scrolled');
+    }
+  }
 
   redirectToLink(elementId: string): void {
-    this.viewportScroller.scrollToAnchor(elementId);
+    const element = document.getElementById(elementId);
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+      inline: 'nearest',
+    });
   }
 }
